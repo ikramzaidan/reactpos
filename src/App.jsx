69cfc38e2464b5556bdf3f94bdef23f3faa8b019
ikramzaidan <ikramzaidan820@gmail.com>
2024-11-07 17:29:42 +0700

    update order list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { FaBars, FaCashRegister, FaClipboardCheck, FaClockRotateLeft, FaGear } from 'react-icons/fa6'
 import ItemCard from './components/ItemCard';
 import Sidebar from './components/Sidebar';
@@ -14,6 +14,7 @@ function App() {
 
   const handleAddItem = (item) => {
     setIsModalOpen(true);
+    setIsSidebarOpen(false);
     setItemSelected(item);
   }
 
@@ -139,20 +140,18 @@ function App() {
       }
       <div className="flex w-full">
         {isModalOpen &&
-          <Modal data={itemSelected} handleClose={() => setIsModalOpen(prev => !prev)} />
+          <Modal data={itemSelected} handleClose={() => setIsModalOpen(prev => !prev)} setOrders={setOrders} />
         }
-        <div className="flex flex-col md:basis-2/3 gap-3 max-h-screen bg-slate-100 p-5 overflow-y-auto overflow-x-hidden">
+        <div className="flex flex-col md:basis-2/3 gap-3 h-full md:max-h-screen bg-slate-100 p-5 overflow-y-auto overflow-x-hidden">
           <div className="flex justify-between items-center gap-5 mb-5">
             <button className="flex justify-center items-center h-full aspect-square hover:bg-slate-200 rounded-md" onClick={() => setIsSidebarOpen(prev => !prev)}>
               <FaBars className="text-xl" />
             </button>
-            <input className="bg-slate-200 rounded-md text-sm p-2" placeholder="Search" />
+            <input name="search" className="bg-slate-200 rounded-md text-sm p-2" placeholder="Search" />
           </div>
           <div className="hidden md:flex items-center gap-2 w-full">
             <div className="bg-white border border-green-600 rounded-full text-green-600 px-5 py-1 cursor-pointer">All</div>
             <div className="bg-white border hover:border-green-600 rounded-full hover:text-green-600 px-5 py-1 cursor-pointer">Favorites</div>
-            <div className="bg-white border hover:border-green-600 rounded-full hover:text-green-600 px-5 py-1 cursor-pointer">Americano Series</div>
-            <div className="bg-white border hover:border-green-600 rounded-full hover:text-green-600 px-5 py-1 cursor-pointer">Coffee</div>
           </div>
           <div className="grid grid-cols-2 lg:grid-cols-4 gap-5">
             {items.map((item, index) => (
@@ -161,35 +160,45 @@ function App() {
           </div>
         </div>
         <div className="hidden md:flex flex-col basis-full md:basis-1/3 justify-between gap-5 bg-white border-l p-5">
-          <div className="flex flex-col gap-3">
-            <div className="flex flex-row border-b pb-3">
-              <div className="flex flex-col basis-4/6">
-                <div className="text-sm font-semibold">Cappucino Butterscotch</div>
-                <div className="text-xs text-gray-500">Iced Medium (2)</div>
-              </div>
-              <div className="basis-2/6 text-end text-sm font-semibold">68.000</div>
+          {orders.length === 0 ? (
+            <div className="flex justify-center items-center h-full text-sm text-gray-600">Belum ada pesanan</div>
+          ) : (
+            <div className="flex flex-col gap-3">
+              {orders.map((order, index) => {
+                const item = items.find((item) => item.id === order.item_id);
+                const variation = item?.variations?.find((variation) => variation.id === order.item_variation_id)
+
+                return(
+                  <div className="flex flex-row border-b pb-3" key={index}>
+                    <div className="flex flex-col basis-4/6">
+                      <div className="text-sm font-semibold">{item.name}</div>
+                      <div className="text-xs text-gray-500">
+                        {variation ? `${variation.name} (${order.item_quantity})` : `(${order.item_quantity})`}  
+                      </div>
+                    </div>
+                    <div className="basis-2/6 text-end text-sm font-semibold">
+                      {(variation ? variation.price * order.item_quantity : item?.price * order.item_quantity).toLocaleString()}
+                    </div>
+                  </div>
+                );
+              })}
             </div>
-            <div className="flex flex-row border-b pb-3">
-              <div className="flex flex-col basis-4/6">
-                <div className="text-sm font-semibold">Cappucino Hazelnut</div>
-                <div className="text-xs text-gray-500">Iced Large</div>
-              </div>
-              <div className="basis-2/6 text-end text-sm font-semibold">39.000</div>
-            </div>
-            <div className="flex flex-row border-b pb-3">
-              <div className="flex flex-col basis-4/6">
-                <div className="text-sm font-semibold">Cappucino Hazelnut</div>
-                <div className="text-xs text-gray-500">Iced Medium</div>
-              </div>
-              <div className="basis-2/6 text-end text-sm font-semibold">34.000</div>
-            </div>
-          </div>
+          )}
+          
           <div className="flex flex-col gap-3 border-t pt-3">
             <div className="flex flex-row border-b pb-3">
               <div className="flex flex-col basis-4/6">
                 <div className="text-sm font-semibold">Total</div>
               </div>
-              <div className="basis-2/6 text-end text-sm font-semibold">Rp 141.000</div>
+              <div className="basis-2/6 text-end text-sm font-semibold">
+                Rp{" "}
+                {orders.reduce((total, order) => {
+                  const item = items.find((item) => item.id === order.item_id);
+                  const variation = item?.variations?.find((variation) => variation.id === order.item_variation_id);
+                  const itemPrice = variation ? variation.price : item?.price;
+                  return total + itemPrice * order.item_quantity;
+                }, 0).toLocaleString()}
+              </div>
             </div>
             <button className="w-full bg-green-600 rounded-md text-white font-bold p-3">Charge</button>
           </div>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,30 @@
-const Modal = ({data, handleClose}) => {
+import { useEffect, useState } from "react";
+
+const Modal = ({data, handleClose, setOrders}) => {
+    const [newOrder, setNewOrder] = useState({});
+
+    useEffect(() => {
+        setNewOrder((prevOrder) => ({
+            ...prevOrder,
+            id: Date.now(),
+            item_id: data.id,
+        }));
+    }, [data]);
+
+    const handleSaveItem = () => {
+        setOrders((prevOrders) => [...prevOrders, newOrder]);
+        handleClose();
+    }
+
+    const handleChangeItem = () => (event) => {
+        const { name, value } = event.target;
+
+        setNewOrder((prevNewOrder) => ({
+            ...prevNewOrder,
+            [name]: value,
+        }));
+    }
+
     return(
         <>
             <div className="fixed w-full h-screen bg-gray-200 opacity-50"></div>
@@ -7,26 +33,30 @@ const Modal = ({data, handleClose}) => {
                     <div className="flex justify-between items-center bg-slate-50 border-b rounded-t rounded-b-none rounded-md p-3">
                         <button className="border border-green-600 rounded text-center text-sm text-green-600 font-semibold px-3 py-2" onClick={handleClose}>Cancel</button>
                         <div className="">{data.name}</div>
-                        <div className="bg-green-600 rounded text-center text-sm text-white font-semibold px-3 py-2">Save</div>
+                        <button className="bg-green-600 rounded text-center text-sm text-white font-semibold px-3 py-2" onClick={() => handleSaveItem()}>Save</button>
                     </div>
                     <div className="flex flex-col gap-3 px-10 py-5">
                         {data.variations && 
                             <div className="flex flex-col gap-1 pb-3 border-b">
                                 <div className="text-sm">Variations</div>
                                 <div className="grid grid-cols-3 gap-5">
-                                    {data.variations.map((variation, index) => (
-                                        <div  key={index} className="flex justify-center hover:bg-green-600 border border-green-600 rounded text-green-600 hover:text-white font-semibold py-2 cursor-pointer">{variation.name}</div>
+                                    {data.variations.map((variation) => (
+                                        <div  key={variation.id} className={`flex justify-center border border-green-600 rounded font-semibold py-2 cursor-pointer
+                                             hover:bg-green-600  hover:text-white ${newOrder.item_variation_id === variation.id ? 'bg-green-600 text-white' : 'text-green-600'}`}
+                                             onClick={() => setNewOrder({...newOrder, item_variation_id: variation.id})}>
+                                            {variation.name}
+                                        </div>
                                     ))}
                                 </div>
                             </div>
                         }
                         <div className="flex flex-col gap-1 pb-3 border-b">
                             <div className="text-sm">Quantity</div>
-                            <input type="number" className="border border-green-600 rounded p-2" placeholder="Qty"></input>
+                            <input name="item_quantity" type="number" className="border border-green-600 rounded px-3 py-2" onChange={handleChangeItem()} placeholder="Quatity" />
                         </div>
                         <div className="flex flex-col gap-1 pb-3 border-b">
                             <div className="text-sm">Notes</div>
-                            <textarea className="border border-green-600 rounded p-2"></textarea>
+                            <textarea name="item_notes" className="border border-green-600 rounded px-3 py-2 resize-none" onChange={handleChangeItem()} placeholder="Add Notes"></textarea>
                         </div>
                     </div>
                 </div>
